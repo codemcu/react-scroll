f33153cf4ad40318b30dbd937797815a712ad342
Mauricio Correa <frontend.mcu@gmail.com>
2020-07-31 13:44:34 +0200

    added Content and Header components

---
+++ src/Content.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, commodi,
+        ipsam voluptate facere odit rem, laborum obcaecati sequi aspernatur esse
+        quia quaerat quisquam minima mollitia qui perferendis fugiat delectus
+        nesciunt.
+      </p>
+    </main>
+  );
+};
+
+export default Content;
