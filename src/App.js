1d6c4a03febbfab84e9de712a0174dd2b3f2069a
Mauricio Correa <frontend.mcu@gmail.com>
2020-08-06 11:51:54 +0200

    funcionality with useEffect, useState, useRef

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
+import React, { useEffect, useState, useRef } from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
 
-import "./App.scss";
+import "./sass/style.scss";
 
 function App() {
+  const [style, setStyle] = useState("show");
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const header = headerRef.current.getBoundingClientRect();
+
+    const handleScrollEvent = () => {
+      let top = window.scrollY;
+      top > header.height ? setStyle("hide") : setStyle("show");
+    };
+
+    window.addEventListener("scroll", handleScrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollEvent);
+    };
+  }, []);
+
   return (
     <div className='app'>
-      <Header></Header>
-      <Content></Content>
+      <header ref={headerRef} className={style}>
+        <Header />
+      </header>
+      <Content />
     </div>
   );
 }
